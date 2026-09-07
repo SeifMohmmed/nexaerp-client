c14a6755431bcaa32fbd67da1cb1b2b37016c36d
Seif Mohamed <saif.eissa7797@gmail.com>
2026-09-07 21:49:47 +0300

    Fix Vercel Suggestion

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.tsx";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
